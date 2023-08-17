f41bea65646615f65a979de11aaba0d6e3d7fb09
KauanCursino <kauan.souza@sptech.school>
2023-08-17 11:28:09 -0300

    vetores

---
+++ JAVA/fundamentos01/src/main/java/scool/sptech/json.java
@@ -0,0 +1,21 @@
+package scool.sptech;
+
+
+import java.io.ObjectStreamException;
+
+public class json {
+    public static void main(String[] args) {
+
+        //NullPointerException é o erro que acontece quando se tenta rodar um código sem declarar uma variável
+
+
+        Integer[] numerosInteiros;
+
+        Integer [] numerosInteiros2 = new Integer[5];
+        Integer[] numerosInteiros3 = {1, 2, 3, 4, 5};
+        String[] nomes = new String[5];
+        Object[] objetos = new Object[5];
+
+
+    }
+}
